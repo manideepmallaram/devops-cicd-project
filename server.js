f22ba34a7f056e3e32b003800d5baa0978f8c4c4
Manideep <manideepmallaram2@gmail.com>
2026-04-28 07:30:17 +0530

    fix server binding for render

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  fs.readFile('index.html', (err, data) => {
+  // Always serve index.html for root
+  let filePath = path.join(__dirname, 'index.html');
+
+  fs.readFile(filePath, (err, data) => {
     if (err) {
-      res.writeHead(404);
-      res.end("Page not found");
+      res.writeHead(500);
+      res.end("Server Error");
     } else {
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(data);
